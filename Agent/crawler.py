48727ff7194fed2bb88a22492290b28b2b334ab2
kr-MATAGI <dev_matagi@kakao.com>
2025-01-20 20:31:39 +0900

    [Add] 실시간 채팅 업데이트 기능 추가

--- Agent/crawler.py
+++ Agent/crawler.py
@@ -51,53 +51,35 @@ class ChatCrawler:
         
         # Crawl live chatting
         time.sleep(3) # Loading
-        # live_chatting_list_wrapper__a5XTV
-        chatting_list_wrapper: WebElement = self.driver.find_element(
-            By.XPATH,
-            "//div[contains(@class, 'live_chatting_list_wrapper')]"
-        )
-        self._logger.debug(f"Chatting list wrapper: {True if chatting_list_wrapper else False}")
-
-        # Get chatting after open browser
-        chat_log_history: Dict = {}
-
-        chat_list_items: List[WebElement] = chatting_list_wrapper.find_elements(
-            By.XPATH, 
-            ".//div[contains(@class, 'live_chatting_list_item')]"
-        )
-        self._logger.debug(f"First Chat Size: {len(chat_list_items)}")
+        total_chat_history: Dict[str, List[Dict]] = self._crawling_chatting()
         
-        for chat_item in chat_list_items:
-            # name_ellipsis__Hu9B+->name_text__yQG50, live_chatting_message_text__DyleH
+        # Live Crawling Loop
+        while True:
+            time.sleep(1)
             
-            try:
-                user_nick_name_elem: WebElement = chat_item.find_element(
-                    By.XPATH,
-                    ".//span[contains(@class, 'name_text')]"
-                )
-                user_chat_elem: WebElement = chat_item.find_element(
-                    By.XPATH,
-                    ".//span[contains(@class, 'live_chatting_message_text')]"
-                )
+            # Update Chatting Log
+            live_chat_items: Dict[str, List[Dict]] = self._crawling_chatting()
 
-                if user_nick_name_elem.text in chat_log_history.keys():
-                    chat_log_history[user_nick_name_elem.text].append({
-                        "content": user_chat_elem.text,
-                        "added_time": datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
-                    })
+            for live_user, live_chat_list in live_chat_items.items():
+                if live_user in total_chat_history.keys():
+                    # 저장된 채팅 내역이 업데이트 내역과 같으면 skip
+                    # @TODO: 'ㅋㅋㅋㅋ' 와 같은 것 어떻게 처리할지 생각
+                    saved_chat_logs: List[str] = [x['content'] for x in total_chat_history[live_user][-50:]]
+                    for chat in live_chat_list:
+                        if chat['content'] in saved_chat_logs:
+                            continue
+
+                        total_chat_history[live_user].append(chat)
+
+                        # TEST
+                        print(f"Extend\n{live_user}, {chat}")
                 else:
-                    chat_log_history[user_nick_name_elem.text] = [{
-                        "content": user_chat_elem.text,
-                        "added_time": datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
-                    }]
+                    # 새롭게 유입된 유저
+                    total_chat_history[live_user] = live_chat_list
 
-            except Exception as e:
-                if user_nick_name_elem:
-                    self._logger.error(f"User: {user_nick_name_elem.text}, {e}")
-            
-        # Test
-        # for k, v in chat_log_history.items():
-        #     print(f"{k}:\n{v}\n\n")
+                    # TEST
+                    print(f"New\n{live_user}, {live_chat_list}")
+        
 
     def _open_website(
         self,
@@ -157,3 +139,54 @@ class ChatCrawler:
                 }
         
         return {}
+    
+
+    def _crawling_chatting(
+        self,
+    ):
+        chat_log_history: Dict[str, List[Dict]] = {}
+
+        # live_chatting_list_wrapper__a5XTV
+        chatting_list_wrapper: WebElement = self.driver.find_element(
+            By.XPATH,
+            "//div[contains(@class, 'live_chatting_list_wrapper')]"
+        )
+        self._logger.debug(f"Chatting list wrapper: {True if chatting_list_wrapper else False}")
+
+        # Get chatting after open browser
+        chat_list_items: List[WebElement] = chatting_list_wrapper.find_elements(
+            By.XPATH, 
+            ".//div[contains(@class, 'live_chatting_list_item')]"
+        )
+        self._logger.debug(f"First Chat Size: {len(chat_list_items)}")
+        
+        for chat_item in chat_list_items:
+            # @TODO: 후원 채팅은 유저는 검색이 됨 '익명의 후원자', 내용과 금액은 다른 Elem 사용하는 것으로 보임
+            # name_ellipsis__Hu9B+->name_text__yQG50, live_chatting_message_text__DyleH
+            
+            try:
+                user_nick_name_elem: WebElement = chat_item.find_element(
+                    By.XPATH,
+                    ".//span[contains(@class, 'name_text')]"
+                )
+                user_chat_elem: WebElement = chat_item.find_element(
+                    By.XPATH,
+                    ".//span[contains(@class, 'live_chatting_message_text')]"
+                )
+
+                if user_nick_name_elem.text in chat_log_history.keys():
+                    chat_log_history[user_nick_name_elem.text].append({
+                        "content": user_chat_elem.text,
+                        "added_time": datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
+                    })
+                else:
+                    chat_log_history[user_nick_name_elem.text] = [{
+                        "content": user_chat_elem.text,
+                        "added_time": datetime.now().strftime("%Y/%m/%d_%H:%M:%S")
+                    }]
+
+            except Exception as e:
+                if user_nick_name_elem:
+                    self._logger.error(f"User: {user_nick_name_elem.text}, {e}")
+
+        return chat_log_history
